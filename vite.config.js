8dbf111d707a31dbab5017fa352abdee2c5d7d47
ssaachin <s5225374@bournemouth.ac.uk>
2023-09-15 19:48:44 +0100

    changes to heroImage

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'https://web-production-42fd.up.railway.app'
-    }
-  },
   plugins: [react()],
 })
