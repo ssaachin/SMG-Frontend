4a7cfbf6fe2965599edf3fb24da6f302d5451eaa
ssaachin <s5225374@bournemouth.ac.uk>
2023-09-15 20:22:42 +0100

    link to dashboard

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import Logo from "../Img/NewLogo.png"
+import React from 'react';
+import Logo from "../Img/NewLogo.png";
+import { Link } from 'react-router-dom'; // Import Link from React Router
 
-function Navbar() {
+export default function Navbar() {
     return (
       <nav className="bg-001525 px-4">
         <div className="container mx-auto flex items-center justify-between">
@@ -9,18 +10,15 @@ function Navbar() {
           <img src={Logo} alt="Logo" className="w-20 h-22" />
   
           <ul className="flex items-center text-white gap-4">
-            <li className="hover:text-red-600">About</li>
+            {/* Use Link component to navigate to /dashboard */}
+            <li><Link to="/dashboard" className="hover:text-red-600">About</Link></li>
             <li href="#treatments" className="hover:text-red-600">Pricing</li>
             <li className="hover:text-red-600">Contact</li>
           </ul>
         </div>
       </nav>
     );
-  }
-  
-  
-  
-   
+}
 
-export default Navbar;
+
 
