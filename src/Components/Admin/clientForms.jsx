6a3c3903c1a1a83b962c13d2a8d9a00a077919fc
ssaachin <s5225374@bournemouth.ac.uk>
2023-09-28 23:50:21 +0100

    remove btn from clientForm

--- src/Components/Admin/clientForms.jsx
+++ src/Components/Admin/clientForms.jsx
@@ -10,11 +10,6 @@ export default function ClientForm() {
                     placeholder="Search..."
                     className="p-2 flex-1 rounded border border-gray-300 focus:border-blue-700 focus:outline-none"
                 />
-                <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded ml-2 focus:outline-none focus:ring focus:ring-blue-200"
-                >
-                    Search
-                </button>
             </div>
         </section>
     );
