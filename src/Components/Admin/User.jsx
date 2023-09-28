09886f4d5cb3ae98bc2206859766d199e39eccc9
ssaachin <s5225374@bournemouth.ac.uk>
2023-09-28 19:48:48 +0100

    better ui

--- src/Components/Admin/User.jsx
+++ src/Components/Admin/User.jsx
@@ -23,23 +23,25 @@ export default function Users() {
     <div className="container mx-auto p-4 sm:p-8">
       <h2 className="text-2xl font-bold mb-4">Clients</h2>
       <div className="max-h-80 overflow-x-auto">
-        <table className="table-auto">
+        <table className="table-auto w-full border-collapse">
           <thead>
             <tr>
-              <th className="px-2 sm:px-4 py-2">Name</th>
-              <th className="px-2 sm:px-4 py-2">Email</th>
-              <th className="px-2 sm:px-4 py-2">Massage Type</th>
-              <th className="px-2 sm:px-4 py-2">Date</th>
-              <th className="px-2 sm:px-4 py-2">Time</th>
+              <th className="px-4 py-2 text-left bg-gray-200">Name</th>
+              <th className="px-4 py-2 text-left bg-gray-200">Email</th>
+              <th className="px-4 py-2 text-left bg-gray-200">Massage Type</th>
+              <th className="px-4 py-2 text-left bg-gray-200">Time and Date</th>
             </tr>
           </thead>
           <tbody>
             {clients.map((client, index) => (
-              <tr key={index}>
-                <td className="border px-2 sm:px-4 py-2">{`${client.first_name} ${client.last_name}`}</td>
-                <td className="border px-2 sm:px-4 py-2">{client.email}</td>
-                <td className="border px-2 sm:px-4 py-2">{client.massage_type}</td>
-                <td className="border px-2 sm:px-4 py-2">{client.time_date}</td>
+              <tr
+                key={index}
+                className={index % 2 === 0 ? 'bg-gray-100' : ''}
+              >
+                <td className="border px-4 py-2">{`${client.first_name} ${client.last_name}`}</td>
+                <td className="border px-4 py-2">{client.email}</td>
+                <td className="border px-4 py-2">{client.massage_type}</td>
+                <td className="border px-4 py-2">{client.time_date}</td>
               </tr>
             ))}
           </tbody>
@@ -48,3 +50,4 @@ export default function Users() {
     </div>
   );
 }
+
